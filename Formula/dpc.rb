0faf7977d63f6243705c2f73afa5eda98dc4b01a
Tormod Haugland <tormod.haugland@gmail.com>
2026-02-10 21:34:48 +0100

    chore: fix dpc formula repo url

--- Formula/dpc.rb
+++ Formula/dpc.rb
@@ -1,15 +1,15 @@
 class Dpc < Formula
   desc "Design Parity Checker - Compare implementations against design references"
-  homepage "https://github.com/tOgg1/design-parity-checker"
+  homepage "https://github.com/trmdy/design-parity-checker"
   version "0.1.0"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/tOgg1/design-parity-checker/releases/download/v#{version}/dpc-aarch64-apple-darwin.tar.gz"
+      url "https://github.com/trmdy/design-parity-checker/releases/download/v#{version}/dpc-aarch64-apple-darwin.tar.gz"
       sha256 "PLACEHOLDER_UPDATE_AFTER_RELEASE"
     else
-      url "https://github.com/tOgg1/design-parity-checker/releases/download/v#{version}/dpc-x86_64-apple-darwin.tar.gz"
+      url "https://github.com/trmdy/design-parity-checker/releases/download/v#{version}/dpc-x86_64-apple-darwin.tar.gz"
       sha256 "PLACEHOLDER_UPDATE_AFTER_RELEASE"
     end
   end
