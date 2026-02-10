e8f7cab34bfde7ad19fc5efb5d56e9ef7ae3d6f3
Tormod Haugland <tormod.haugland@gmail.com>
2026-02-10 21:28:05 +0100

    feat: add dpc formula

---
+++ Formula/dpc.rb
@@ -0,0 +1,25 @@
+class Dpc < Formula
+  desc "Design Parity Checker - Compare implementations against design references"
+  homepage "https://github.com/tOgg1/design-parity-checker"
+  version "0.1.0"
+  license "MIT"
+
+  on_macos do
+    if Hardware::CPU.arm?
+      url "https://github.com/tOgg1/design-parity-checker/releases/download/v#{version}/dpc-aarch64-apple-darwin.tar.gz"
+      sha256 "PLACEHOLDER_UPDATE_AFTER_RELEASE"
+    else
+      url "https://github.com/tOgg1/design-parity-checker/releases/download/v#{version}/dpc-x86_64-apple-darwin.tar.gz"
+      sha256 "PLACEHOLDER_UPDATE_AFTER_RELEASE"
+    end
+  end
+
+  def install
+    bin.install "dpc"
+  end
+
+  test do
+    system "#{bin}/dpc", "--help"
+  end
+end
+
